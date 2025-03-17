15721c1f0dea3dc1faa414da67c7f58434e92902
JeongWonYoung <nicednjsdud@naver.com>
2025-03-17 11:19:01 +0900

    Refactor: MapleUsers -> MapleUserTable

--- src/main/kotlin/app/domain/maple/repository/MapleRepository.kt
+++ src/main/kotlin/app/domain/maple/repository/MapleRepository.kt
@@ -1,9 +1,7 @@
 package app.domain.maple.repository
 
 import app.domain.maple.dto.MapleUserCreateRequest
-import app.domain.maple.dto.MapleUserUpdateRequest
-import app.domain.maple.model.mapleUser.MapleUser
-import app.domain.maple.model.mapleUser.MapleUsers
+import app.domain.maple.model.mapleUser.MapleUserTable
 import kotlinx.coroutines.Dispatchers
 import org.jetbrains.exposed.sql.Database
 import org.jetbrains.exposed.sql.insert
@@ -14,32 +12,33 @@ import org.jetbrains.exposed.sql.update
 class MapleRepository(private val db: Database){
 
     suspend fun isExists(userId: Int): Boolean = newSuspendedTransaction(Dispatchers.IO, db) {
-        MapleUsers.select { MapleUsers.userId eq userId }
+        MapleUserTable.select { MapleUserTable.userId eq userId }
             .empty().not() // ✅ 데이터가 있으면 true, 없으면 false 반환
     }
 
    suspend fun insert(mapleUser: MapleUserCreateRequest, userId: Int) = newSuspendedTransaction(Dispatchers.IO, db) {
-        MapleUsers.insert {
-            it[ocid] = mapleUser.ocid
-            it[name] = mapleUser.name
-            it[worldName] = mapleUser.worldName
-            it[classLevel] = mapleUser.classLevel
-            it[characterClass] = mapleUser.characterClass
-            it[level] = mapleUser.level
-            it[exp] = mapleUser.exp
-            it[expRate] = mapleUser.expRate
-            it[guildName] = mapleUser.guildName
-            it[imageUrl] = mapleUser.imageUrl
-            it[gender] = mapleUser.gender
-            it[createdDate] = mapleUser.createdDate
-            it[isAccessible] = mapleUser.isAccessible
-            it[liberationQuestClear] = mapleUser.liberationQuestClear
-            it[MapleUsers.userId] = userId
-        }
+       MapleUserTable.insert {
+           it[ocid] = mapleUser.ocid
+           it[name] = mapleUser.name
+           it[worldName] = mapleUser.worldName
+           it[classLevel] = mapleUser.classLevel
+           it[characterClass] = mapleUser.characterClass
+           it[level] = mapleUser.level
+           it[exp] = mapleUser.exp
+           it[expRate] = mapleUser.expRate
+           it[guildName] = mapleUser.guildName
+           it[imageUrl] = mapleUser.imageUrl
+           it[gender] = mapleUser.gender
+           it[createdDate] = mapleUser.createdDate
+           it[isAccessible] = mapleUser.isAccessible
+           it[liberationQuestClear] = mapleUser.liberationQuestClear
+           it[searchDate] = mapleUser.searchDate
+           it[userId] = userId
+       }
     }
 
     suspend fun update(mapleUser: MapleUserCreateRequest, userId: Int) = newSuspendedTransaction(Dispatchers.IO, db) {
-        MapleUsers.update({ MapleUsers.userId eq userId }) {
+        MapleUserTable.update({ MapleUserTable.userId eq userId }) {
             it[name] = mapleUser.name
             it[worldName] = mapleUser.worldName
             it[classLevel] = mapleUser.classLevel
